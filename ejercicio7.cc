2c218bfb7e85563a68e3319d2aa1726007ceeba3
patric16UCM <93123535+patric16UCM@users.noreply.github.com>
2021-11-08 10:33:50 +0100

    Create ejercicio7.cc

---
+++ ejercicio7.cc
@@ -0,0 +1,23 @@
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+char *path= argv[1];
+ int resultado =umask(0020);
+ if(resultado == -1){
+     perror("Se ha producido un error fijando la mascara.");
+ }
+  resultado = open(path,O_CREAT|O_TRUNC);
+
+ if(resultado == -1){
+     perror("Se ha producido un error creando el archivo.");
+ }
+ return 1;   
+}
