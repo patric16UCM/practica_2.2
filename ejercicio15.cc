babc7e0ae57addb6511524fbe1bce6ae96e0afa9
patric16UCM <93123535+patric16UCM@users.noreply.github.com>
2021-11-11 11:38:19 +0100

    Update ejercicio15.cc

--- ejercicio15.cc
+++ ejercicio15.cc
@@ -3,7 +3,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <errno.h>
+#include <time.h>
 #include <string.h>
 
 using namespace std;
@@ -11,12 +11,19 @@ using namespace std;
 int main(int argc, char *argv[]){
 char *path= argv[1];
 int fd = open(path,O_RDWR);
-if(lockf(fd,F_TEST,100) == 0){
+if(lockf(fd,F_TEST,100) != 0){
     lockf(fd,F_LOCK,100);
+    time_t tiempo = time(NULL);
+    struct tm *tm = localtime(&tiempo);
+    printf("bloqueado \n");
+    printf("hora actual: %d:%d \n",tm->tm_hour,tm->tm_min);
     sleep(30);
     lockf(fd,F_ULOCK,100);
+    
 }
-
+else{
+    printf("Bloqueado \n");
+}
 //si esta desbloqueado, fijaara un cerrojo y escribira la hora actual.
 
 
