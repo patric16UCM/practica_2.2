14f9e6394d263d1350df7cb7cce9dbe63368784f
patric16UCM <93123535+patric16UCM@users.noreply.github.com>
2021-11-12 15:50:31 +0100

    Create ejercicio12.cc

---
+++ ejercicio12.cc
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+char *path= argv[1];
+int fd = open(path,O_CREAT,0777);//lo creo con todos los permisos
+int fd2 = dup2(fd,1);
+printf("rediccionamiento");
+dup2(fd2,fd);
+
+
+ return 1;   
+}
